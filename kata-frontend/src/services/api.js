6d251e8982187cde9fa88edceb98850d8e2adf9e
demo_user <sharmapiyush10718@gmail.com>
2025-07-22 10:38:01 +0530

    Trying Deployment on vercel.

--- kata-frontend/src/services/api.js
+++ kata-frontend/src/services/api.js
@@ -2,9 +2,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:5000/api",
+  baseURL: "http://localhost:5000/api",
+  // baseURL: import.meta.env.VITE_BASE_URL,
   // baseURL: "https://sweetshop-backend.onrender.com/api",
-  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 export default api;
